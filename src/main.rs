b002706ec967caceaf4cfd71ee933809f8ffa27b
Sommos <Sommosgaming@gmail.com>
2023-06-16 18:52:21 +0100

    added hash_to_binary and is_block_valid functions to main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,20 @@ pub struct Block {
     pub nonce: u64,
 }
 
+const DIFFICULTY_PREFIX: &str = "00";
+
+// convert a hash to binary
+fn hash_to_binary(hash: &[u8]) -> String {
+    // create an empty string
+    let mut res: String = String::default();
+    // iterate over the hash
+    for c in hash {
+        res.push_str(&format!("{:b}", c));
+    }
+    // return the binary string
+    res
+}
+
 impl App {
     fn new() -> Self {
         Self{
@@ -24,6 +38,7 @@ impl App {
         }
     }
 
+    // create the genesis block
     fn genesis(&mut self) {
         // save the current timestamp
         let timestamp = UTC::now().timestamp();
@@ -56,5 +71,32 @@ impl App {
         };
         // push the genesis block to the blocks vector
         self.blocks.push(genesis_block);
+    }
+
+    // add a new block to the blockchain
+    fn add_new_block(&mut self, block: Block) {
+        // get the latest block
+        let latest_block = self.blocks.last().expect("there is at least one block");
+        // check if the block is valid
+        if self.is_block_valid(&block, latest_block) {
+            // push the block to the blocks vector
+            self.blocks.push(block);
+        // if the block is not valid, log an error
+        } else {
+            error!("invalid block");
+        }
+    }
+
+    // check if the block is valid
+    fn is_block_valid(&self, block: &block, previous_block: &block) -> bool {
+        if block.previous_hash != previous_block.hash {
+            warn!("block with id: {} has wrong previous hash", block.id);
+            return false;
+        } else if !hash_to_binary(&hex::decode(&block.hash).expect("can decode from hex")).starts_with(DIFFICULTY_PREFIX) {
+            warn!("block with id: {} has invalid difficulty", block.id);
+            return false;
+        } else if block.id != previous_block.id + 1 {
+            warn!("block with id: {} is not the next block after previous block with id: {}", block.id, previous_block.id);
+        }
     }
 }
